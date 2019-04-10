c543676dba535131b965c45c1de9ffbebb198b9b
Gersen James <gersen.jorge@gmail.com>
2019-04-10 17:43:00 -0300

    Criando as rotas do módulo Calculadora

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './components';
 import { CalculadoraService } from './services';
+import { CalculadoraRoutingModule } from './calculadora-routing.modules';
+import { CalculadoraRoutingComponent } from './calculadora-routing.component';
 
 @NgModule({
   declarations: [
     CalculadoraComponent,
+    CalculadoraRoutingComponent,
   ],
   imports: [
     CommonModule,
+    CalculadoraRoutingModule,
   ],
   exports:[
     CalculadoraComponent,
