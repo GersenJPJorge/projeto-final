4aab77c621bb5827ae9d1648516c4f6450723a9b
Gersen James <gersen.jorge@gmail.com>
2019-04-10 15:52:57 -0300

    Configurando as rotas de navegação no componente principal

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+export const routes: Routes = [
+/*	{ 
+		path: '',                                   // na inicialização ou se não for digitado nada na url
+		redirectTo: '/dashboard', 
+		pathMatch: 'full'                           // passa a url completa
+    },
+    */
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],  // forRoot indica que ele é único, que controlará todos as rotas da aplicação
+  exports: [ RouterModule ]                   // com o exporte ele fica disponível para o módulo principal
+})
+export class AppRoutingModule {}
